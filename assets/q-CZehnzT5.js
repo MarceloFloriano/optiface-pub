83317e5d763518d7f845bcea8545fd090b830722
lu1znogueira <luizetn@gmail.com>
2025-10-02 16:25:31 -0300

    2025.1002.1625

--- assets/q-CZehnzT5.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index-DuBwiQTG.js";const t=a(async({app:o})=>{globalThis.$q=o.config.globalProperties.$q});export{t as default};
---
+++ assets/q-D9CWSJir.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index-Dp3kLygd.js";const t=a(async({app:o})=>{globalThis.$q=o.config.globalProperties.$q});export{t as default};
