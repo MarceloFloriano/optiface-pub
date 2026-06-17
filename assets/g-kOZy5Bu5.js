2891f2cbbe3ea785347e62d98942cd647a9b11d3
Lucas <lucassouzacps19@gmail.com>
2026-06-17 14:02:14 -0300

    2026.0617.1401

--- assets/g-kOZy5Bu5.js
+++
@@ -1,1 +0,0 @@
-import{bO as t}from"./index-nI_4aQ8c.js";const o={},a=t(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{a as default};
--- assets/router-05-cSpgH.js
+++
@@ -1,1 +0,0 @@
-import{bO as t}from"./index-nI_4aQ8c.js";const a=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{a as default};
