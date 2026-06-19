57eae0540998f86f3c61fe095af20f31d9da3be6
luiznogueira <luizetn@gmail.com>
2026-06-19 16:59:48 -0300

    2026.0619.1659

---
+++ assets/g-7rZoty4v.js
@@ -0,0 +1,1 @@
+import{bO as t}from"./index-B65vseup.js";const o={},a=t(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{a as default};
---
+++ assets/utils-D-QUa-yR.js
@@ -0,0 +1,1 @@
+import{u as o}from"./index-Btn4yfgZ.js";import{bO as i}from"./index-B65vseup.js";import"./atendimento-6dZVdr0I.js";import"./date-BU2NFQwP.js";const m=i(({app:t})=>{t.config.globalProperties.$utils=o,globalThis.$utils=o});export{m as default};
