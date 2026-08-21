5d08b959c1867e0360d81970e892ad04810e6ed1
Lucas <lucassouzacps19@gmail.com>
2026-08-21 12:02:11 -0300

    2026.0821.1159

--- assets/lodash-BQD4oY1w.js
+++ assets/lodash-BQD4oY1w.js
@@ -1,4 +1,4 @@
-import{cw as Qe,cx as up,bO as fp}from"./index-bkwP4DL5.js";var ne={exports:{}};/**
+import{cx as Qe,cy as up,bO as fp}from"./index-etpMkO6p.js";var ne={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
--- assets/router-DH6EpRB1.js
+++
@@ -1,1 +0,0 @@
-import{bO as t}from"./index-bkwP4DL5.js";const a=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{a as default};
