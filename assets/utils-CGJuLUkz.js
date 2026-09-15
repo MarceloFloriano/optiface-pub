c9df0fd92d64e79248630c751c57a216c4057ca3
luiznogueira <luizetn@gmail.com>
2026-09-14 22:32:50 -0300

    2026.0914.2232

---
+++ assets/utils-CGJuLUkz.js
@@ -0,0 +1,1 @@
+import{u as o}from"./index-CWnwDn1j.js";import{bO as i}from"./index-CEU50BM6.js";import"./atendimento-Ixxg8Ypk.js";import"./date-D7f4AkHU.js";const m=i(({app:t})=>{t.config.globalProperties.$utils=o,globalThis.$utils=o});export{m as default};
